c7e593473b4131837d82f8328d178a0ff4044e20
DeepeshKalura <deepeshkalurs@gmail.com>
2025-04-14 20:23:00 +0530

    Update link text in App component from "Band App" to "Band Tape"

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Band App
+          Band Tape
         </a>
       </header>
     </div>
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,6 @@ import App from '../App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Band App/i);
+  const linkElement = screen.getByText(/Band Tape/i);
   expect(linkElement).toBeInTheDocument();
 });
